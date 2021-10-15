4d012df9b45c134accc39d532fb23593fb42bd39
Miguel Pérez-Torres <torres@iaa.es>
2021-10-15 12:02:16 +0200

    Added SPIworkflow

---
+++ SPIworkflow/constants.py
@@ -0,0 +1,22 @@
+# Useful constants
+c   = 2.99792458e10        #  speed of light, in cm/s
+m_e = 9.1093897e-28              # electron mass, in g
+e   = 4.803204712570263e-10      # electron charge (esu)
+k_B = 1.380658e-16           # Boltzmann's constant (erg/K)
+m_h = 1.673534e-24               # Hydrogen mass, in g
+
+pc = pc2cm   = 3.0857e18              # 1 pc, in cm
+au      = 1.49597871e+13         # astonomical unit, in cm
+mJy     = 1.0e-26                # 1 mJy, in erg/s/cm2/Hz
+
+G     = 6.67408e-8              # Gravitational constant (cgs)
+M_sun = Msun = 1.9891e33                # Mass of Sun, in g
+R_sun = Rsun = 6.9599e10              # Radius of the Sun, in cm
+M_earth = 5.9742e+27     # g
+R_earth = 6.378136e+8    # cm
+M_jup = M_jupiter  = 1.8987e+30 # in g
+R_jup = R_jupiter  = 7.1492000e+9  # in cm
+
+yr = 365.24 #yr, in days
+day = 86400. #day,in seconds
+yr2sec = yr*day #yr, in seconds
