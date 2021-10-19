460f6259097545cafbd7bb5a19d79fe8cc56ce82
Miguel Pérez-Torres <torres@iaa.es>
2021-10-19 09:32:41 +0200

    Added

---
+++ SPIworkflow/wind_density.py
@@ -0,0 +1,16 @@
+
+def n_wind(M_star_dot=3e-14, d=7e10, v_sw=25.6e5):
+    """ Computes the particle density of the stellar wind at some distance d from the
+        center of the star.
+        OUTPUT: n_sw - particle density of the stellar wind at distance d, in #/cm^3
+        INPUT:  M_star_dot - stellar mass-loss rate, M_sun/yr
+                d          - distance from the center of the star, in cm
+                v_sw       - Speed of stellar wind at distance d, in cm/s
+    """
+    from SPIworkflow.constants import *
+
+    M_star_dot *= M_sun/yr2sec  # mass-loss rate, in grams/sec
+    m_av  =  1.92e-24 # average particle mass of the solar wind, in grams
+    rho = M_star_dot/(4*np.pi * d**2 * v_sw*1e5) # Density of the stellar wind, in gr/cm3
+    n_sw = rho / m_av
+    return n_sw
