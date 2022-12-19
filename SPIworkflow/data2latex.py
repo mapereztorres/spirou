a1e4490ae4e5079b045e16bff0e8651e6130727a
Miguel Pérez-Torres <torres@iaa.es>
2022-12-19 10:53:02 +0100

    Deleted files

--- SPIworkflow/data2latex.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def get_latex_table(data='./INPUT/SPI-sources.csv'):
-    df = pd.DataFrame(data, columns = ['planet name', 'RA (h:m:s)', 'DEC (d:m:s)', 'd_pc', 'P_rot', 'planet_radius (R_Earth)', 'orbital_period(days)'])
-    for indi in [0, 5, 31, 33, 89, 90, 91]:
-        with open('sources.tex', 'w') as tf:
-            tf.write(df.to_latex(index=False))
