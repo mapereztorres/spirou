7135d0a6dab76c328cab76b32a06bf62e66622d4
luispena96 <luipenmon@gmail.com>
2023-04-12 09:58:26 +0200

    Fixed label position with respect to axes

--- SPI.py
+++ SPI.py
@@ -481,8 +481,11 @@ for indi in star_array:
             #ax1.set_xscale('log')
             #ax2.set_xscale('log')
             #VALORES ORIGINALES
-            ax1.set_ylim([-3,0])
-            ax2.set_ylim([-3,3])
+            ymin_ax1=-3
+            ymin=-3
+            ymax=3
+            ax1.set_ylim([ymin_ax1,0])
+            ax2.set_ylim([ymin,ymax])
             #VALORES TEST
             #ax1.set_ylim([-50,20])
             #ax2.set_ylim([-50,80])
@@ -545,9 +548,10 @@ for indi in star_array:
             #ax2.text(x=12,y=-1.6,s=r"$B_{\rm planet}$ = " + str(Bp) + " G ", fontsize='small')
             #ax2.text(x=12,y=-2.0,s=r"$n_{\rm corona}$ = " + str(n_sw_base/1e7) + "x10$^7$ cm$^{-3}$ ", fontsize='small')
             #ax2.text(x=3,y=0.1+np.log10(3*rms),s=r"Requested 3$\sigma$", fontsize='x-small')
-            xpos = d_orb_max / 2 + 0.7+7.5
-            ypos = 1.5+0.7
-            d_ypos = 0.5
+            xpos =xmax*0.8
+            ypos_offset = (ymax-ymin)/8
+            ypos = (ymax-ymin)/4 + ypos_offset
+            d_ypos = (ymax-ymin)/12
             ax2.text(x=xpos,y=ypos,s=r"$B_\star$    = " + str(B_star) + " G ",fontsize='small')
             ax2.text(x=xpos,y=ypos-d_ypos,s=r"$B_{\rm planet}$ = " + str(B_pl_loc) + r"$B_{\rm Earth}$", fontsize='small')
             ax2.text(x=xpos,y=ypos-2*d_ypos, s=r"$n_{\rm corona}$ = " + str(n_sw_base/1e7) + "x10$^7$ cm$^{-3}$ ", fontsize='small')
