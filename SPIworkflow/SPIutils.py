86a542d691f142f351067f451450faeb292bb624
Miguel Pérez-Torres <torres@iaa.es>
2023-04-28 18:39:03 +0200

    SPIutils.py - added function plasma_frequency

--- SPIworkflow/SPIutils.py
+++ SPIworkflow/SPIutils.py
@@ -79,4 +79,11 @@ def Lrad_leto(B_star=1.0, R_star=1.0, P_rot=1.0):
     Ltot_rad = Lnu_rad * nu_ecm
     return Ltot_rad
     
-
+def plasma_freq(n_e = 1.0):
+    """ Returns the plasma frequency.
+        Any wave emitting at a frequency less than the plasma frequency will
+        not propate in this medium.
+        OUTPUT: nu_plasma (plasma frequency), in Hz
+        INPUT : n_e - electron density, in cm^(-3)
+    """
+    nu_plasma = 9.0e3 * np.sqrt(n_e)
