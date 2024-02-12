c78aba9c79c863e7e5bf691197bf8336804ccf63
Miguel Pérez-Torres <torres@iaa.es>
2024-02-12 10:49:49 +0100

    Added function wind_composition to calculate X_e, mu and m_av.

--- SPIworkflow/SPIutils.py
+++ SPIworkflow/SPIutils.py
@@ -144,7 +144,7 @@ def Lrad_leto(B_star=1.0, R_star=1.0, P_rot=1.0):
     Ltot_rad = Lnu_rad * nu_ecm
     return Ltot_rad
     
-def v_stellar_wind(d_orb, M_star, T_corona):
+def v_stellar_wind(d_orb, M_star, T_corona, m_av):
     """Computes the stellar wind speed at every distance (d_orb) from the star
        It assumes an isothermal Parker wind 
        OUTPUT:
@@ -174,22 +174,34 @@ def v_stellar_wind(d_orb, M_star, T_corona):
 
     return v_sw
     
-def n_wind(M_star_dot=1.0, d=7e10, v_sw=25.6e5, m_av = m_av):
+def wind_composition(X_p = 0.5):
+    """ Computes fraction of electrons, mean "molecular weight" and average particle
+    mass of the stellar wind, given the fraction of protons.
+    OUTPUT: X_e - fraction of electrons of the stellar wind
+            mu  - mean "molecular" weight of the stellar wind
+            m_av - average particle mass of the stellar wind (in gr)
+    INPUT : X_p - fraction of protons
+    """
+    X_e = 1 - X_p
+    mu = (X_p * m_p + X_e * m_e)/(m_p + m_e) 
+    m_av = mu * m_p  
+
+    return X_e, mu, m_av
+
+def n_wind(M_star_dot=1.0, d=7e10, v_sw=25.6e5, m_av = 0.5 * m_p):
     """ Computes the particle density of the stellar wind at some distance d from the
         center of the star.
         OUTPUT: n_sw - particle density of the stellar wind at distance d, in #/cm^3
         INPUT:  M_star_dot - stellar mass-loss rate, in units of the Sun mass-loss rate
                 d          - distance from the center of the star, in cm 
                 v_sw       - Speed of stellar wind at distance d, in cm/s
-                m_av       - average mass density of the stellar wind 
+                m_av       - average particle mass of the stellar wind (= mu * m_p) in grams
     """
     M_sun_dot = 2e-14 # Sun mass-loss rate, in Msun/yr
     M_star_dot *= M_sun_dot * M_sun/yr2sec  # mass-loss rate, in grams/sec
-    """
-    m_av  =  mu * m_p # average particle mass of the solar wind, in grams
-    """
     rho  = M_star_dot/(4*np.pi * d**2 * v_sw) # Density of the stellar wind, in gr/cm3
     n_sw = rho / m_av
+
     return n_sw
 
 def get_alfven(rho_sw_planet, B_sw, B_r, v_rel, v_sw):
